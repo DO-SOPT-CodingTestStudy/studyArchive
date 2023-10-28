3ed8a097fbfad232972ce1327c2ee1d55c19ae87
Eunggwan Kim <40496065+alpha-kwhn@users.noreply.github.com>
2023-10-28 17:46:47 +0900

    Delete SQL directory

--- SQL/prog_level2_상품별 온라인 매출 구하기.sql
+++
@@ -1,7 +0,0 @@
-select p.product_code, sum(s.sales_amount) * p.price as sales
-from product p
-right outer join offline_sale s
-on p.product_id = s.product_id
-where s.sales_amount > 0 and p.product_id 
-group by p.product_code
-order by sales desc, product_code asc
