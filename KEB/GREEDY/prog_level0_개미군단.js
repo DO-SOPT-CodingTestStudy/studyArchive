76109dde1246a3a73c0be4cdd774ac161eddcd10
Genesis <dmsgktn1685@naver.com>
2023-10-29 22:30:26 +0900

    fork 레포지토리 재설정

--- KEB/GREEDY/prog_level0_개미군단.js
+++
@@ -1,8 +0,0 @@
-// [Lv.0]
-
-function solution(hp) {
-  let j = Math.floor(hp / 5);
-  let b = Math.floor((hp - j * 5) / 3);
-  let i = hp - j * 5 - b * 3;
-  return j + b + i;
-}
--- KEK/SQL/prog_level2_카테고리 별 상품 개수 구하기.sql
+++
@@ -1,3 +0,0 @@
-select substring(product_code, 1, 2) as category, count(product_id) as products
-from product
-group by category
